7309e3b0e11ec3cf8299de062cbc09c71484fc29
alyssrod <125409694+alyssrod@users.noreply.github.com>
2024-04-26 13:42:07 -0400

    Add files via upload

---
+++ js/final_exam_js_component.js
@@ -0,0 +1,37 @@
+/*
+  Do NOT uncomment the lines of code below, they will cause errors.  
+  But I am providing it as a template.
+  */
+
+/* 
+document.querySelector("ELEMENT").addEventListener("EVENT",function(){
+     //YOUR CODE
+ })
+ */
+
+
+
+// Problem 1
+
+
+// Problem 2
+
+
+// Problem 3
+// Part 1
+
+
+// Problem 3
+// Part 2
+
+
+
+// Problem 4
+
+
+
+// Problem 5
+
+
+// Problem 6
+
